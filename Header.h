13ed012ba2f72de6e8503f6322a4bb942be8d9ea
Pablo Sandoval <pabloarmandosandoval@gmail.com>
2018-10-13 04:16:02 -0500

    vs commit

---
+++ Header.h
@@ -0,0 +1,193 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include <type_traits>
+#include <cassert>
+using namespace std;
+
+template<class T>
+class stackADT
+{
+public:
+	virtual void initializeStack() = 0;
+	///method to initialize the stack to an empty state
+	///postcodition: Stack is empty
+
+	virtual bool isEmptyStack() const = 0;
+	///Function to determine whether the stack is empty
+	///postcondtion: Return the true if the stack is empty
+	///				 otherwise returns false
+
+	virtual bool isFullStack() const = 0;
+	///Function to determine whether the stack is full.
+	///Postcondition: Returns true if the stack is full,
+	///otherwise returns false.
+	
+	virtual void push(const T& newItem) = 0;
+	///Function to add newItem to the stack.
+	///Precondition: The stack exists and is not full.
+	///Postcondition: The stack is changed and newItem
+	///is added to the top of the stack.
+
+	virtual T top() const = 0;
+	///Function to return the top element of the stack.
+	///Precondition: The stack exists and is not empty.
+	///Postcondition: If the stack is empty, the program
+	///terminates; otherwise, the top element
+	///of the stack is returned.
+
+	virtual void pop() = 0;
+	///Function to remove the top element of the stack.
+	///Precondition: The stack exists and is not empty.
+	///Postcondition: The stack is changed and the top
+};
+
+template <class U>
+class stackType : public stackADT<U>
+{
+public:
+	const stackType<U>& operator=(const stackType<U>&);
+	///Overload the assignment operator.
+
+	void initializeStack();
+	///Function to initialize the stack to an empty state.
+	///Postcondition: stackTop = 0;
+
+	bool isEmptyStack() const;
+	///Function to determine whether the stack is empty.
+	///Postcondition: Returns true if the stack is empty,
+	///otherwise returns false.
+
+	bool isFullStack() const;
+	///Function to determine whether the stack is full.
+	///Postcondition: Returns true if the stack is full,
+	///otherwise returns false.
+
+	void push(const U& newItem);
+	///Function to add newItem to the stack.
+	///Precondition: The stack exists and is not full.
+	///Postcondition: The stack is changed and newItem
+	///is added to the top of the stack.
+
+	U top() const;
+	///Function to return the top element of the stack.
+	///Precondition: The stack exists and is not empty.
+	///Postcondition: If the stack is empty, the program
+	///terminates; otherwise, the top element
+	///of the stack is returned.
+
+	void pop();
+	///Function to remove the top element of the stack.
+	///Precondition: The stack exists and is not empty.
+	///Postcondition: The stack is changed and the top
+	///element is removed from the stack.
+
+	stackType(int stackSize = 100);
+	///Constructor
+	///Create an array of the size stackSize to hold
+	///the stack elements. The default stack size is 100.
+	///Postcondition: The variable list contains the base
+	///address of the array, stackTop = 0, and
+	///maxStackSize = stackSize.
+
+	stackType(const stackType<U>& otherStack);
+	//Copy constructor
+
+	~stackType();
+	///Destructor
+	///Remove all the elements from the stack.
+	///Postcondition: The array (list) holding the stack
+	///elements is deleted.
+
+private:
+	int maxStackSize; ///variable to store the maximum stack size
+	int stackTop; ///variable to point to the top of the stack
+	U *list; ///pointer to the array that holds the
+			 ///stack elements
+	void copyStack(const stackType<U>& otherStack);
+	///Function to make a copy of otherStack.
+	///Postcondition: A copy of otherStack is created and
+	///assigned to this stack.
+};
+
+template <class U>
+void stackType<U>::initializeStack() { stackTop = 0; } ///end of the stack
+
+
+template <class U>
+stackType<U>::stackType(int stackSize)
+{
+	list = new U[stackSize]; ///the stack holds up to 100 items
+	stackTop = 0;
+	maxStackSize = stackSize; ///set max size of the stack to the stack size
+}
+
+template <class U>
+bool stackType<U>::isEmptyStack() const
+{
+	if (stackTop == 0) ///returns true if the stack is empty, otherwise
+		return true;
+
+	else
+		return false;
+}
+
+template <class U>
+bool stackType<U>::isFullStack() const
+{
+	if (stackTop == maxStackSize) ///if the stack is full returns true, otherwise
+		return true;
+	else
+		return false;
+}
+
+template <class U>
+void stackType<U>::push(const U& newItem)
+{
+	if (!isFullStack())
+	{
+		list[stackTop] = newItem; ///add newItem at the top
+		++stackTop; ///increment stackTop
+	}
+
+	else
+		cout << "Stack is full\n";
+}
+
+template <class U>
+U stackType<U>::top() const
+{
+	if (stackTop != 0)
+		return list[stackTop - 1];
+
+	else
+	{
+		cout << "Stack is empty\n";
+		return NULL;
+	}
+	///assert(stackTop != 0); this makes the program stop and opens up window to abort
+
+}
+
+template <class U>
+void stackType<U>::pop()
+{
+	if(!isEmptyStack())
+	{
+		auto previous = list[stackTop - 1]; ///I'm setting U previous to be set to anything from the stacktop - 1
+		stackTop--; ///decrementing stack, popping the stack
+		cout << previous << endl; ///outputting what was popped
+	}
+}
+
+template <class U>
+void stackType<U>::copyStack(const stackType<U>& otherStack)
+{
+	
+}
+
+template <class U>
+stackType<U>::~stackType()
+{
+	delete[]list;
+}
---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	clock_t const start = clock();
+	double const duration = (clock() - start) / static_cast<double>(CLOCKS_PER_SEC);
+
+	cout << "hello\n";
+	stackType<int> myStack;
+	myStack.push(1);
+	myStack.push(30);
+
+	cout << myStack.top() << endl;
+	cout << myStack.isEmptyStack() << endl;
+	cout << myStack.isFullStack() << endl;
+	myStack.pop();
+	myStack.pop();
+	cout << myStack.top() << endl;
+	stackType<string> mystackT;
+	mystackT.push("Hello");
+	mystackT.push("there");
+	cout << mystackT.top() << endl;
+	stackType<char> charstack;
+	charstack.push('m');
+	charstack.push('e');
+	cout <<charstack.top() << endl;
+	charstack.pop();
+	charstack.pop();
+	cout << charstack.top() << endl;
+	cout << "CLOCKS_PER_SEC: " << duration << '\n';
+	return 0;
+}
